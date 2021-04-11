8746c614c25efe3729a0acbff7778278d7de7b22
chekni <karina_cheshko@mail.ru>
2021-04-11 22:29:21 +0300

    feat: add solution for 4st task

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -21,8 +21,65 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+function minesweeper(m) {
+  const gameArea = new Array(m.length);
+  for (let i = 0; i < gameArea.length; i++) {
+    gameArea[i] = new Array(m[0].length);
+    gameArea[i].fill(0);
+  }
+
+  for (let i = 0; i < m.length; i += 1) {
+    for (let j = 0; j < m[0].length; j += 1) {
+      if (i === 0 && j === 0) {
+        gameArea[i][j] += +m[i][j + 1];
+        gameArea[i][j] += +m[i + 1][j] + +m[i + 1][j + 1];
+      } else
+      if (i === 0 && j === m[0].length - 1) {
+        gameArea[i][j] += +m[i][j - 1];
+        gameArea[i][j] += +m[i + 1][j] + +m[i + 1][j - 1];
+      } else
+      if (i === m.length - 1 && j === 0) {
+        gameArea[i][j] += +m[i][j + 1];
+        gameArea[i][j] += +m[i - 1][j] + +m[i - 1][j + 1];
+      } else
+      if (i === m.length - 1 && j === m[0].length - 1) {
+        gameArea[i][j] += +m[i - 1][j] + +m[i - 1][j - 1];
+        gameArea[i][j] += +m[i][j - 1];
+      } else
+      // верхняя строка матрицы
+      if (i === 0 && j !== 0 && j !== m[0].length - 1) {
+        gameArea[i][j] += +m[i][j - 1] + +m[i][j + 1];
+        gameArea[i][j] += +m[i + 1][j - 1] + +m[i + 1][j] + +m[i + 1][j + 1];
+      } else
+      // нижняя строка матрицы
+      if (i === m.length - 1 && j !== 0 && j !== m[0].length - 1) {
+        gameArea[i][j] += +m[i][j - 1] + +m[i][j + 1];
+        gameArea[i][j] += +m[i - 1][j - 1] + +m[i - 1][j] + +m[i - 1][j + 1];
+      } else
+      // левый столбец матрицы
+      if (j === 0 && i !== 0 && i !== m.length - 1) {
+        gameArea[i][j] += +m[i - 1][j] + +m[i - 1][j + 1] + m[i][j + 1];
+        gameArea[i][j] += +m[i + 1][j] + +m[i + 1][j + 1];
+      } else
+      // правый столбец матрицы матрицы
+      if (j === m[0].length - 1 && i !== 0 && i !== m.length - 1) {
+        gameArea[i][j] += +m[i - 1][j - 1] + +m[i - 1][j];
+        gameArea[i][j] += +m[i][j - 1] + +m[i + 1][j] + +m[i + 1][j - 1];
+      } else {
+        gameArea[i][j] += +m[i + 1][j - 1] + +m[i + 1][j] + +m[i + 1][j + 1];
+        gameArea[i][j] += +m[i][j - 1] + +m[i][j + 1];
+        gameArea[i][j] += +m[i - 1][j - 1] + +m[i - 1][j] + +m[i - 1][j + 1];
+      }
+    }
+  }
+
+  return gameArea;
 }
 
 module.exports = minesweeper;
+
+// minesweeper([
+//     [true, false, false],
+//     [false, true, false],
+//     [false, false, false],
+// ]);
