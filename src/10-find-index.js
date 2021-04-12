38b7baaa5dd07709fd85ac49643d8ff147a7a2b5
chekni <karina_cheshko@mail.ru>
2021-04-12 12:37:55 +0300

    feat: add solution for 10 task

--- src/10-find-index.js
+++ src/10-find-index.js
@@ -11,8 +11,21 @@
  * For ([1, 2, 3], 2) should return 1
  *
  */
-function findIndex(/* array, value */) {
-  throw new Error('Not implemented');
+function findIndex(array, value) {
+  let leftBorder = 0;
+  let rightBorder = array.length - 1;
+  while (leftBorder <= rightBorder) {
+    const middlePosition = Math.ceil((leftBorder + rightBorder) / 2);
+    if (array[middlePosition] === value) {
+      return middlePosition;
+    }
+    if (array[middlePosition] > value) {
+      rightBorder = middlePosition - 1;
+    } else {
+      leftBorder = middlePosition;
+    }
+  }
+  return leftBorder;
 }
 
 module.exports = findIndex;
