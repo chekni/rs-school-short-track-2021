4354b7825d3c6277c6367d3e0d1359493d31004f
chekni <karina_cheshko@mail.ru>
2021-04-12 13:16:57 +0300

    feat: add solution for 11 task

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,25 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  if (str.length) {
+    let result = '';
+    let counterRepeat = 1;
+    let prev = str[0];
+    for (let i = 1; i < str.length; i += 1) {
+      const cur = str[i];
+      if (cur === prev) {
+        counterRepeat += 1;
+      } else {
+        result += counterRepeat > 1 ? `${counterRepeat}${prev}` : prev;
+        prev = str[i];
+        counterRepeat = 1;
+      }
+    }
+    result += counterRepeat > 1 ? `${counterRepeat}${prev}` : prev;
+    return result;
+  }
+  return '';
 }
 
 module.exports = encodeLine;
