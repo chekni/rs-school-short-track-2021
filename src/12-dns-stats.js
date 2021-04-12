0ed3b4aaa19496812f6ea35a7181329e7bb8d2a6
chekni <karina_cheshko@mail.ru>
2021-04-12 17:07:44 +0300

    feat: add solution for 12 task

--- src/12-dns-stats.js
+++ src/12-dns-stats.js
@@ -20,8 +20,24 @@
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new Error('Not implemented');
+function getDNSStats(domains) {
+  const map = new Map();
+  const separator = '.';
+  for (let i = 0; i < domains.length; i += 1) {
+    const array = domains[i].split(separator).reverse();
+    for (let j = 0; j < array.length; j += 1) {
+      let k = 0;
+      let domain = '';
+      while (k <= j) {
+        domain += separator + array[k];
+        k++;
+      }
+      if (map.has(domain)) {
+        map.set(domain, map.get(domain) + 1);
+      } else map.set(domain, 1);
+    }
+  }
+  return Object.fromEntries(map);
 }
 
 module.exports = getDNSStats;
